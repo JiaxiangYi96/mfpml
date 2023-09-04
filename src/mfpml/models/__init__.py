0dc5f5cdc9003183a2453e1a16be23ccd0776e96
Jiaxiang Yi <88609987+JiaxiangYi96@users.noreply.github.com>
2023-09-04 11:28:21 +0200

    add gp known noise

--- src/mfpml/models/__init__.py
+++ src/mfpml/models/__init__.py
@@ -2,4 +2,4 @@ from .co_kriging import CoKriging
 from .hierarchical_kriging import HierarchicalKriging
 from .kernels import RBF, KernelCore, kronDelta
 from .mf_scale_kriging import ScaledKriging
-from .sf_gpr import GaussianProcessRegressor, Kriging
+from .sf_gpr import GaussianProcessRegressor, GPKnownNoise, Kriging
--- src/mfpml/models/sf_gpr.py
+++ src/mfpml/models/sf_gpr.py
@@ -558,3 +558,212 @@ class GaussianProcessRegressor(GP):
         #              np.sum(np.log(np.diag(self.L)))).item()
         self.logp = -0.5 * np.dot(self.sample_y.T, self.alpha) - np.sum(
             np.log(np.diag(self.L))) - 0.5*self.num_samples*np.log(2*np.pi)
+
+
+class GPKnownNoise(GP):
+    """
+    Gaussian Process Regressor with noise, and the noise is already known,
+    in this case, the noise is not optimized
+    """
+
+    def __init__(
+        self,
+        design_space: np.ndarray,
+        optimizer: Any = None,
+        kernel_bound: list = [-4.0, 3.0],
+        known_noise: float = 1.0,
+    ) -> None:
+        """Initialize the Gaussian Process Regressor
+
+        Parameters
+        ----------
+        design_space : np.ndarray
+            array of shape=((num_dim,2)) where each row describes
+            the bound for the variable on specific dimension
+        optimizer : any, optional
+            instance of the optimizer used to optimize the hyper parameters
+            with the use style optimizer.run_optimizer(objective function,
+            number of dimension, design space of variables), if not assigned,
+            the 'L-BFGS-B' method in scipy is used
+        kernel_bound : list, optional
+            log bound for the Kriging kernel, by default [-4, 3]
+        known_noise : float, optional
+            , by default 0
+        """
+        # dimension of the modeling problem
+        self.num_dim = design_space.shape[0]
+        # bounds of the design space
+        self.bounds = design_space
+        # optimizer
+        self.optimizer = optimizer
+        # noise
+        self.noise = known_noise
+        # kernel
+        self.kernel = RBF(theta=np.zeros(self.num_dim), bounds=kernel_bound)
+
+    def predict(self,
+                x_predict: np.ndarray,
+                return_std: bool = False):
+        """Predict responses through the Gaussian Process Regressor
+
+        Parameters
+        ----------
+        x_predict : np.ndarray
+            new sample need to predict
+        return_std : bool, optional
+            whether return the standard deviation
+            , by default False
+
+        Returns
+        -------
+        np.ndarray
+            return the prediction with shape (#Xinput, 1)
+        """
+        # normalize the input
+        sample_new = self.normalize_input(x_predict, self.bounds)
+        sample_new = np.atleast_2d(sample_new)
+        # calculate the kernel matrix for predicted samples
+        knew = self.kernel.get_kernel_matrix(self.sample_scaled_x, sample_new)
+        # get the predicted mean
+        fmean = self.mu + np.dot(knew.T, self.gamma)
+        fmean = fmean.reshape(-1, 1)
+        # calculate the standard deviation
+        if not return_std:
+            return fmean
+        else:
+            one = np.ones((self.num_samples, 1))
+            delta = solve(self.L.T, solve(self.L, knew))
+            # epistemic uncertainty
+            try:
+                mse = self.sigma2 * (1 - np.diag(np.dot(knew.T, delta)) +
+                                     np.diag((1 - knew.T.dot(delta)) ** 2
+                                             / one.T.dot(self.beta)))
+            except Exception:
+                print("The matrix is too big, use for loop to calculate")
+                mse = np.zeros((knew.shape[1], 1))
+                batch = 100
+                iter = knew.shape[1] / batch
+                for i in range(int(iter)):
+                    try:
+                        knew_i = knew[:, i * batch: (i+1)*batch]
+                        delta_i = solve(self.L.T, solve(self.L, knew_i))
+                        mse[i * batch: (i+1) * batch] = self.sigma2 * \
+                            (1 - np.diag(np.dot(knew_i.T, delta_i)) +
+                                np.diag((1 - knew_i.T.dot(delta_i)) ** 2
+                                        / one.T.dot(self.beta))).reshape(-1, 1)
+                    except Exception:
+                        # remain part of the matrix
+                        knew_i = knew[:, i * batch:]
+                        delta_i = solve(self.L.T, solve(self.L, knew_i))
+                        mse[i * batch:] = self.sigma2 * \
+                            (1 - np.diag(np.dot(knew_i.T, delta_i)) +
+                                np.diag((1 - knew_i.T.dot(delta_i)) ** 2
+                                        / one.T.dot(self.beta))).reshape(-1, 1)
+            # aleatoric uncertainty
+            data_noise = self.noise**2
+
+            # total uncertainty
+            std = np.sqrt(np.maximum(mse+data_noise, 0)).reshape(-1, 1)
+            return fmean, std
+
+    def _hyper_paras_optimization(self, grads: bool = None) -> None:
+        """Optimize the hyper_parameters
+
+        Parameters
+        ----------
+        grads : bool, optional
+            whether to use gradients, by default None
+        """
+        # set up the optimization problems
+        lower_bound = self.kernel._get_low_bound
+        upper_bound = self.kernel._get_high_bound
+        # bounds for the hyper-parameters
+        hyper_bounds = np.vstack((lower_bound, upper_bound)).T
+        # number of hyper-parameters
+        num_hyper = self.kernel._get_num_para
+
+        if self.optimizer is None:
+            n_trials = 10
+            opt_fs = float("inf")
+            for _ in range(n_trials):
+                x0 = np.random.uniform(
+                    low=lower_bound,
+                    high=upper_bound,
+                    size=num_hyper,
+                )
+                optRes = minimize(
+                    self._logLikelihood,
+                    x0=x0,
+                    method="L-BFGS-B",
+                    bounds=hyper_bounds,
+                )
+                if optRes.fun < opt_fs:
+                    opt_param = optRes.x
+                    opt_fs = optRes.fun
+        else:
+            optRes, _, _ = self.optimizer.run_optimizer(
+                self._logLikelihood,
+                num_dim=num_hyper,
+                design_space=hyper_bounds,
+            )
+
+            opt_param = optRes["best_x"]
+        # best hyper-parameters
+        self.opt_param = opt_param
+
+    def _logLikelihood(self, params: np.ndarray) -> np.ndarray:
+        """Compute the concentrated ln-likelihood
+
+        Parameters
+        ----------
+        params : np.ndarray
+            parameters of the kernel
+
+        Returns
+        -------
+        np.ndarray
+            log likelihood
+        """
+        params = np.atleast_2d(params)
+        num_params = params.shape[1]
+        out = np.zeros(params.shape[0])
+
+        for i in range(params.shape[0]):
+            # for optimization every row is a parameter set
+            param = params[i, 0: num_params]
+
+            # correlation matrix R
+            # calculate the covariance matrix
+            K = self.kernel(self.sample_scaled_x,
+                            self.sample_scaled_x,
+                            param) + np.eye(self.num_samples) * self.noise**2
+            #
+            L = cholesky(K, lower=True)
+            # R^(-1)Y
+            alpha = solve(L.T, solve(L, self.sample_y))
+
+            logp = -0.5 * np.dot(self.sample_y.T, alpha) - np.sum(
+                np.log(np.diag(L))) - 0.5*self.num_samples*np.log(2*np.pi)
+
+            out[i] = logp.ravel()
+
+        return -out
+
+    def _update_kernel_matrix(self) -> None:
+        # assign the best hyper-parameter to the kernel
+        self.kernel.set_params(self.opt_param[0:len(self.opt_param)])
+        # update parameters with optimized hyper-parameters
+        self.K = self.kernel.get_kernel_matrix(
+            self.sample_scaled_x,
+            self.sample_scaled_x) + np.eye(self.num_samples) * self.noise**2
+        self.L = cholesky(self.K, lower=True)
+        self.alpha = solve(self.L.T, solve(self.L, self.sample_y))
+        one = np.ones((self.num_samples, 1))
+        self.beta = solve(self.L.T, solve(self.L, one))
+        self.mu = (np.dot(one.T, self.alpha) / np.dot(one.T, self.beta)).item()
+        self.gamma = solve(self.L.T, solve(self.L, (self.sample_y - self.mu)))
+        self.sigma2 = (
+            np.dot((self.sample_y - self.mu).T, self.gamma) / self.num_samples
+        ).item()
+        self.logp = -0.5 * np.dot(self.sample_y.T, self.alpha) - np.sum(
+            np.log(np.diag(self.L))) - 0.5*self.num_samples*np.log(2*np.pi)
