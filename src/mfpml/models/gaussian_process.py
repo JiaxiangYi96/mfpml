41b6c46f241d0e3cebd3dc6f05eca8a4107190cc
yaga <yagafighting@gmail.com>
2023-10-07 19:54:37 +0200

    fix bug on gpr model

--- src/mfpml/models/gaussian_process.py
+++ src/mfpml/models/gaussian_process.py
@@ -225,7 +225,7 @@ class GaussianProcess(GP):
 
             # step 3: calculate the log likelihood
             logp = -0.5 * self.num_samples * \
-                np.log(sigma2) - np.sum(np.log(np.diag(K)))
+                np.log(sigma2) - np.sum(np.log(np.diag(L)))
             nll[i] = -logp.ravel()
 
         return nll
@@ -265,4 +265,4 @@ class GaussianProcess(GP):
 
         # step 3: get the optimal log likelihood
         self.logp = (-0.5 * self.num_samples * np.log(self.sigma2) -
-                     np.sum(np.log(np.diag(self.K)))).item()
+                     np.sum(np.log(np.diag(self.L)))).item()
