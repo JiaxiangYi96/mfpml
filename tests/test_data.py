eee81e80824de758307f6a663dc45c8d486fb560
Ji Cheng <jicheng9617@gmail.com>
2024-02-29 22:46:00 +0800

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,12 +2,12 @@ import numpy as np
 import pytest 
  
 
-from mfpml.core import mfArray 
+from mfpml import mfArray 
 
 def test_mfArray() -> None: 
-    np.random.seed(123)
+    mfarray1 = mfArray(num_fidelity=3) 
+    assert mfarray1 == [None] * 3
+    
     data = mfArray([np.random.rand(4,2), np.random.rand(8,2)]) 
-
-    assert data.n_levels == 2 
+    assert data.num_fidelity == 2 
     np.random.seed(123)
-    assert data[0] == np.random.rand(4,2)
